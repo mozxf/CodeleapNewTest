857423d1874828be87792c0052f8c1cab6ad8bf7
mozxf <nandogustavo2001*gmail.com>
2022-09-07 15:40:12 -0300

    fixing logo path in homepage, try n 3

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import styles from './Homepage.module.scss';
 import { Link } from 'react-router-dom';
-
-const codeleapLogo = '/assets/codeleap-logo-black.svg';
+import logo from '/src/assets/codeleap-logo-black.svg';
 
 export const Homepage = () => {
   return (
     <section className={styles.background}>
       <img
         className={styles.codeleapLogo}
-        src={codeleapLogo}
+        src={logo}
         alt='logo codeleap preto'
       />
       <Link className={styles.accessButton} to='/signup'>
